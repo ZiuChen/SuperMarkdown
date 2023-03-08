b9c3926b5b1f9d1071b65c8ca972a321cfcb9a7e
ZiuChen <ZiuChen@outlook.com>
2023-03-08 22:01:50 +0800

    feat: 引入usePageBack钩子函数，用于添加左上角返回按钮

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="setting">
+    <BackButton></BackButton>
     <h1>Setting</h1>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { usePageBack } from '@/hooks/usePageBack'
 
-<style lang="less" scoped></style>
+const { BackButton } = usePageBack()
+</script>
+
+<style lang="less" scoped>
+.back {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+}
+</style>
