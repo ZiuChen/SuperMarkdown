7c66d0e2a297e1f9630a4dd9dcf010f386bd08f4
ZiuChen <ZiuChen@outlook.com>
2023-04-03 16:47:49 +0800

    fix: 对运行环境进行验证 防止被逆向
    - 验证isElectron环境与SERVER_DEV环境

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,17 @@ import router from './router'
 import { createPinia } from 'pinia'
 import { registerIcon } from '@/common/registerIcon'
 import { registerCallback } from '@/common/registerCallback'
-import { isElectron } from './utils'
+import { isElectron, SERVER_DEV } from './utils'
+;(function () {
+  try {
+    if (!isElectron) throw new Error('unsupported platform') // 禁用非 electron 环境的运行
+    if (!SERVER_DEV && utools.isDev()) return // 禁用非热更新服务器的开发模式的运行 防止逆向
 
-try {
-  registerCallback()
-  const pinia = createPinia()
-  createApp(App).use(router).use(pinia).use(registerIcon).mount('#app')
-} catch (error) {
-  if (isElectron) utools.showNotification('初始化失败: ' + error)
-}
+    registerCallback()
+    const pinia = createPinia()
+    createApp(App).use(router).use(pinia).use(registerIcon).mount('#app')
+  } catch (error) {
+    console.log(error)
+    if (isElectron) utools.showNotification('初始化失败: ' + error)
+  }
+})()
