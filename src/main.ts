4776db77caf68c5ecb7e81871b60df0e674491cc
ZiuChen <ZiuChen@outlook.com>
2023-04-22 23:32:18 +0800

    chore: 清理冗余代码

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,10 @@ import router from './router'
 import { createPinia } from 'pinia'
 import { registerIcon } from '@/common/registerIcon'
 import { registerCallback } from '@/common/registerCallback'
-import { isElectron, SERVER_DEV } from './utils'
+import { isElectron } from './utils'
 
 try {
-  // if (!SERVER_DEV && !isElectron) throw new Error('unsupported platform') // 禁用非 electron 环境的运行
-  // if (!SERVER_DEV && utools.isDev()) throw Error('unsupported enviroment') // 禁用非热更新服务器的开发模式的运行 防止逆向
-
-  const pinia = createPinia()
-  createApp(App).use(router).use(pinia).use(registerIcon).mount('#app')
-
+  createApp(App).use(router).use(createPinia()).use(registerIcon).mount('#app')
   registerCallback()
 } catch (error) {
   if (isElectron) utools.showNotification('初始化失败: ' + error)
